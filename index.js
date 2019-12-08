cbe18fa9648a57168a2c7052bf7f7823a2d1d065
gilons <gf.694765457@gmail.com>
2019-12-08 09:20:49 +0100

    more functions added

--- index.js
+++ index.js
@@ -24,54 +24,81 @@ function addMessageTyeToNofication(type) {
 }
 exports.informOthers = functions.https.onRequest((request, response) => {
     //admin.database().ref('messages').push(request.query.key)
-    admin.database().ref(`rooms/${request.query.activity_id}/${request.query.room_name}`).once('value', (snapshoot) => {
+    admin.database().ref(`rooms/${request.query.activity_id}/${request.query.room_key}`).once('value', (snapshoot) => {
         let room = snapshoot.val()
-        let message = request.query.message || "no caption added"
-        let members = room.members
-        members.map(element => {
-            let phone = request.query.sender_phone.replace(" ","+")
-            if (element.phone !== phone) {
-                admin.database().ref(`current_room/${element.phone}`).once('value', snapshoot => {
-                    let current_room = snapshoot.val()
-                    if (current_room !== request.query.room_key) {
-                        admin.database().ref(`notifications_tokens/${element.phone}`).once('value', snap => {
-                            let token = snap.val()
-                            //console.log(token)
-                            let ref = admin.database().ref(`new_message/${element.phone}/${request.query.room_key}`)
-                            ref.once('value', snapValue => {
-                                if (snapValue.val() === null) {
-                                    ref.set({ new_messages: [request.query.message_key] })
-                                } else {
-                                    ref.set({ new_messages: [...snapValue.val().new_messages, request.query.message_key] });
-                                }
-                                if (token) {
-                                    const payload = {
-                                        notification: {
-                                            title: "New " + addMessageTyeToNofication(request.query.message_type) + " from " +
-                                                request.query.room_name + "@" +
-                                                request.query.activity_name,
-                                            body: message,
-                                        },
-                                        data: {
-                                            "room_key": request.query.room_key,
-                                            "type": "new_message_activity",
-                                            "activity_id": request.query.activity_id
-                                        }
-                                    }
-                                    admin.messaging().sendToDevice(token, payload).then((status) => {
-                                        console.log(status)
-                                        return true
-                                    }).catch(error => {
-                                        console.log(error)
+        if (room !== null) {
+            let message = request.query.message || "no caption added"
+            let members = room.members
+            members.map(element => {
+                let phone = request.query.sender_phone.replace(" ", "+")
+                let memberPhone = element.phone.replace("00", "+")
+                admin.database.ref(`activity/${activity_id}/participants`).once('value', mem => {
+                    if (mem.findIndex(ele => ele.phone.replace("00", "+") === memberPhone) >= 0) {
+                        if (memberPhone !== phone) {
+                            admin.database().ref(`current_room/${memberPhone}`).once('value', snapshoot => {
+                                let current_room = snapshoot.val()
+                                if (current_room !== request.query.room_key) {
+                                    admin.database().ref(`notifications_tokens/${memberPhone}`).once('value', snap => {
+                                        let token = snap.val()
+                                        //console.log(token)
+                                        let ref = admin.database().ref(`new_message/${activity_id}/${memberPhone}/${request.query.room_key}`)
+                                        ref.once('value', snapValue => {
+                                            if (snapValue.val() === null) {
+                                                ref.set({ new_messages: [request.query.message_key] })
+                                            } else {
+                                                ref.set({ new_messages: [...snapValue.val().new_messages, request.query.message_key] });
+                                            }
+                                            if (token) {
+                                                const payload = {
+                                                    notification: {
+                                                        title: "New " + addMessageTyeToNofication(request.query.message_type) + " from " +
+                                                            request.query.room_name + "@" +
+                                                            request.query.activity_name,
+                                                        body: message,
+                                                    },
+                                                    data: {
+                                                        "room_key": request.query.room_key,
+                                                        "type": "new_message_activity",
+                                                        "activity_id": request.query.activity_id
+                                                    }
+                                                }
+                                                admin.messaging().sendToDevice(token, payload).then((status) => {
+                                                    console.log(status)
+                                                    return true
+                                                }).catch(error => {
+                                                    console.log(error)
+                                                })
+                                            }
+                                        })
                                     })
+                                    return "ok"
                                 }
                             })
-                        })
-                        return "ok"
+                        }
                     }
                 })
-            }
-        })
+            })
+        }
+    })
+    return response.status(200).send("okooo")
+})
+
+exports.addParticipant = functions.https.onRequest((request, response) => {
+    let member = JSON.parse(request.query.members)
+    let event_id = request.query.event_id
+    admin.database().ref(`activity/${event_id}/participants`).once('value', snap => {
+        let newMembers = snap.val() !== null ? snap.val().concat(member) : member
+        admin.database().ref(`activity/${event_id}/participants`).set(newMembers)
+    })
+    return response.status(200).send("okooo")
+})
+
+exports.removeParticipant = functions.https.onRequest((request, response) => {
+    let member = JSON.parse(request.query.members)
+    let event_id = request.query.event_id
+    admin.database().ref(`activity/${event_id}/participants`).once('value', snap => {
+        let newMembers = snap.val() !== null ? snap.val().filter(ele => member.indexOf(ele.phone) < 0) : []
+        admin.database().ref(`activity/${event_id}/participants`).set(newMembers)
     })
     return response.status(200).send("okooo")
 })
